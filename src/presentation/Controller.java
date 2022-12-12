ddbe911ca84cabb8b8a194fe2c346f40868be9bd
tonicorral <antoni.corral@students.salle.url.edu>
2022-12-12 20:07:53 +0100

    hola

--- src/presentation/Controller.java
+++ src/presentation/Controller.java
@@ -1,4 +1,5 @@
 package presentation;
+import java.lang.Math;
 
 public class Controller {
     private Menu menu;
@@ -53,20 +54,60 @@ public class Controller {
     private boolean characterCreation(){
         String name;
         String playerName;
+        int level;
         menu.showMessage("Tavern keeper: “Oh, so you are new to this land.”\n");
         menu.showMessage("“What’s your name?”\n\n");
-        menu.showMessage("-> Enter your name:");
+        menu.showMessage("-> Enter your name: ");
         name = menu.askForString();
-        menu.showMessage("Tavern keeper: “Hello,"+ name +", be welcome.”\n");
+        menu.showMessage("Tavern keeper: “Hello,"+ name +", be welcome.\n");
         menu.showMessage("“And now, if I may break the fourth wall, who is your Player?”\n\n");
-        menu.showMessage("-> Enter the player’s name:");
+        menu.showMessage("-> Enter the player’s name: ");
         playerName = menu.askForString();
-        menu.showMessage(playerName);
+       // menu.showMessage(playerName);
+        menu.showMessage("\nTavern keeper: “I see, I see...”");
+        menu.showMessage("\n“Now, are you an experienced adventurer?”\n");
+        menu.showMessage("\n-> Enter the character’s level [1..10]: ");
+        level = menu.askForInt();
+        menu.showMessage("\nTavern keeper: “Oh, so you are level "+level+"!”");
+        menu.showMessage("\n“Great, let me get a closer look at you...”\n");
+        menu.showMessage("\nGenerating your stats...\n\n");
+
+        int num = (int)(Math.random()*10+1);
+
         return true;
     }
 
     private boolean listCharacters(){
-        System.out.println("2");
+        menu.showMessage("2");
+        menu.showMessage("\nTavern keeper: “Lads! They want to see you!”");
+        menu.showMessage("“Who piques your interest?”\n");
+        menu.showMessage("\n-> Enter the name of the Player to filter: ");
+        String playerName=menu.askForString();
+        menu.showMessage("\nYou watch as some adventurers get up from their chairs and approach you.\n");
+
+        //aqui printaremos los que tengan el nombre del jugador igual
+        //while(){
+        //}
+        menu.showMessage("\n0. Back\n");
+        menu.showMessage("Who would you like to meet [0..5]: ");
+        int character=menu.askForInt();
+        menu.showMessage("");
+
+        menu.showMessage("Name: ");
+        menu.showMessage("Player: ");
+        menu.showMessage("Class: ");
+        menu.showMessage("Level: ");
+        menu.showMessage("XP: ");
+        menu.showMessage("Body: ");
+        menu.showMessage("Mind: ");
+        menu.showMessage("Spirit: ");
+
+        menu.showMessage("[Enter name to delete, or press enter to cancel]");
+        menu.showMessage("Do you want to delete "++"?");
+
+        menu.showMessage("Tavern keeper: “I’m sorry kiddo, but you have to leave.”");
+
+        menu.showMessage("Character "++" left the Guild.");
         return true;
     }
 
